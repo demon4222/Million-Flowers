560087810db5788b7fed668e4785c4a703c252e6
Dima Osadchuk <dima12osadchuk@gmail.com>
2020-03-06 14:28:48 +0200

    home adds removed

--- resources/views/layouts/homeAdds.blade.php
+++ resources/views/layouts/homeAdds.blade.php
@@ -1,34 +1,34 @@
-<section class="block my-5">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="p-3 text-center">
-                <div class="card" style="border:none">
-                    <div class="card-body my-2">
-                        <h2 class="card-text text-uppercase" style="font-size: 20px">Індивідуальне оформлення</h2>
-                    </div>
-                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 text-center">
-                <div class="card" style="border:none">
-                    <div class="card-body my-2">
-                        <p class="card-text text-uppercase" style="font-size: 20px">Фото перед відправкою</p>
-                    </div>
-                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 text-center">
-                <div class="card" style="border:none">
-                    <div class="card-body my-2">
-                        <h2 class="card-text text-uppercase" style="font-size: 20px">Листівка</h2>
-                    </div>
-                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+{{--<section class="block my-5">--}}
+{{--    <div class="row">--}}
+{{--        <div class="col-md-4">--}}
+{{--            <div class="p-3 text-center">--}}
+{{--                <div class="card" style="border:none">--}}
+{{--                    <div class="card-body my-2">--}}
+{{--                        <h2 class="card-text text-uppercase" style="font-size: 20px">Індивідуальне оформлення</h2>--}}
+{{--                    </div>--}}
+{{--                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-4">--}}
+{{--            <div class="p-3 text-center">--}}
+{{--                <div class="card" style="border:none">--}}
+{{--                    <div class="card-body my-2">--}}
+{{--                        <p class="card-text text-uppercase" style="font-size: 20px">Фото перед відправкою</p>--}}
+{{--                    </div>--}}
+{{--                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-4">--}}
+{{--            <div class="p-3 text-center">--}}
+{{--                <div class="card" style="border:none">--}}
+{{--                    <div class="card-body my-2">--}}
+{{--                        <h2 class="card-text text-uppercase" style="font-size: 20px">Листівка</h2>--}}
+{{--                    </div>--}}
+{{--                    <img src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</section>--}}
