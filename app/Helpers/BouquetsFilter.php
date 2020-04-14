28774bffb8b0a1566c10a0c56ffc3773c8ee60ed
Dima Osadchuk <dima12osadchuk@gmail.com>
2020-04-14 20:46:37 +0300

    fixes filter

--- app/Helpers/BouquetsFilter.php
+++ app/Helpers/BouquetsFilter.php
@@ -33,7 +33,7 @@ class BouquetsFilter
         if ((isset($this->filters()['_url'])&&(count($this->filters()) == 1) || (count($this->filters()) == 2 && isset($this->filters()['page'])))) {
             $this->builder = $this->builder->paginate($this->perPage);
         }
-        $this->builder = $this->builder->paginate($this->perPage);
+        //$this->builder = $this->builder->paginate($this->perPage);
         return $this->builder;
     }
 
